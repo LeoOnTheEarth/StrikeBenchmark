5fcde7a599a6a1f28c0e6ecffac3055dd607bb09
Leo <leo.on.the.earth@gmail.com>
2014-07-31 02:13:09 +0800

    updated "symfony-cache/index.php"

--- symfony-cache/index.php
+++ symfony-cache/index.php
@@ -3,19 +3,19 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\Routing\Router;
-use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Loader\PhpFileLoader;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpFoundation\Request;
 
 $locator = new FileLocator(array(__DIR__ . '/../cases'));
-$requestContext = new RequestContext($_SERVER['REQUEST_URI']);
 
 $router = new Router(
     new PhpFileLoader($locator),
     'symfony-routes.php',
-    array('cache_dir' => __DIR__ . '/../cache'),
-    $requestContext
+    array(
+        'cache_dir' => __DIR__ . '/../cache',
+        'matcher_cache_class' => 'ProjectUrlMatcher',
+    )
 );
 
 $parameters = $router->matchRequest(Request::createFromGlobals());
